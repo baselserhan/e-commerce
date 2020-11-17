340ff8c57966a90c350faa1bca1164114211114d
Mohammed Abu Shaban <mohammed.eabushaban@gmail.com>
2020-11-17 10:04:59 +0200

    new Edit

---
+++ src/config/config.js
@@ -0,0 +1,16 @@
+require('env2')('.env');
+
+console.log(process.env)
+
+module.exports = {
+  "development": {
+    username: 'wcazrglzlrtgih',
+    password: '590e497e22c5bcafdf12e1f3c83388619db679e5a9a5d7c00e2640791f160fe3',
+    database: 'd5uaq1u4ela7kr',
+    host: 'ec2-52-31-233-101.eu-west-1.compute.amazonaws.com',
+    'dialect': 'postgres',
+    "dialectOptions": {
+      "ssl": true
+    }
+  },
+};
